d5554dccb4708e7183833e9ee6447b0ca4e59a28
StarryBei <hb794426422@gmail.com>
2024-02-26 18:48:22 +0800

    实现1*1的小物品随机摆放

--- RandomEventPlacement.js
+++ RandomEventPlacement.js
@@ -2,51 +2,115 @@
  * @target MZ
  * @plugindesc Random Event Placement Plugin for RPG Maker MZ
  * @StarryBei
- * @command changeImage
+ * @command randomPlacement
  * 
- * @arg eventId
+ * @arg regionPlace
+ * @text Region Place
+ * @desc The region to place the items in.
+ * @type number[]
+ * @default [0, 0, 0, 0]
+ * 
+ * @arg items
+ * @text Items
+ * @desc The list of items to be randomly placed.
+ * @type struct<Item>[]
+ * @default []
+ *
+ * @help RandomEventPlacement.js
+ *
+ * This plugin randomly places items in a specified region when the player enters a room.
+ */
+
+/*~struct~Item:
+ * @param eventId
  * @text Event ID
  * @desc The ID of the event to change the image for.
  * @type number
  * @default 1
- 
- * @arg imageList
- * @text Image List
- * @desc A list of image filenames to choose from (e.g., ["Actor1", "Actor2"]).
- * @type text[]
- * @default ["Actor1","Actor2","Actor3"]
- *
- * @help RandomEventPlacement.js
- *
- * This plugin randomly changes the image of a selected event on the current map.
+ * 
+ * @param image
+ * @text Image
+ * @desc The image contains tiles to show.
+ * @type text
+ * @default !Kitchen1
+ * 
+ * @param imageIdx
+ * @text Image Index
+ * @desc The index of the image to show.
+ * @type number
+ * @default 0
  */
 
 (() => {
     const pluginName = 'RandomEventPlacement';
 
-    // 注册插件命令
-    PluginManager.registerCommand(pluginName, 'changeImage', args => {
-        const eventId = Number(args.eventId);
-        const imageList = JSON.parse(args.imageList);
-        changeEventImage(eventId, imageList);
+    PluginManager.registerCommand(pluginName, 'randomPlacement', args => {
+        const regionPlace = JSON.parse(args.regionPlace).map(Number);
+        const items = JSON.parse(args.items);
+        randomPlaceItems(regionPlace, items);
     });
 
+    function getRandomNumberFromList(numbers) {
+        var randomIndex = Math.floor(Math.random() * numbers.length);
+        var randomNumber = numbers[randomIndex];
+        return randomNumber;
+    }
+
+    function getRandomNumberFromRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
     // 函数用于随机更改事件图像
-    function changeEventImage(eventId, imageList) {
+    function changeEventImage(eventId, image, imageIdx) {
         const event = $gameMap.event(eventId);
         if (event) {
-            const randomIndex = Math.floor(Math.random() * imageList.length);
-            const imageName = imageList[randomIndex];
-            const imageIndex = 0; // 如果你的素材有多个图像在同一文件中，这里可以更改索引
-            event.setImage(imageName, imageIndex);
+            event.setImage(image, imageIdx);
+            event.setDirectionFix(false);
+            const direction = getRandomNumberFromList([2, 4, 6, 8]);
+            event.setDirection(direction);
+            event.setDirectionFix(true);
+            event.setStepAnime(true);
+            console.log("changeEventImage");
         }
     }
 
-    // 插件命令：随机更改指定事件的图像
-    PluginManager.registerCommand(pluginName, 'changeImage', args => {
-        const eventId = Number(args.eventId);
-        const imageList = JSON.parse(args.imageList);
-        changeEventImage(eventId, imageList);
+    function changeEventPostion(eventId, x, y) {
+        const event = $gameMap.event(eventId);
+        if (event) {
+            event.setPosition(x, y);
+        }
+    }
+
+    // 随机摆放物品
+    function randomPlaceItems(regionPlace, items) { // regionPlace[x1, x2, y1, y2]
+        const availableItems = [...items]
+        console.log(regionPlace[0], regionPlace[1], regionPlace[2], regionPlace[3]);
+        const width = Math.abs(regionPlace[1] - regionPlace[0]) + 1;
+        const height = Math.abs(regionPlace[3] - regionPlace[2]) + 1;
+        const placeableCoords = Array(width*height).fill(true)
+        availableItems.forEach(item => {
+            item = JSON.parse(item);
+            const eventId = item.eventId;
+            const image = item.image;
+            const imageIdx = item.imageIdx;
+            changeEventImage(eventId, image, imageIdx);
+            let x, y;
+            do {
+                x = getRandomNumberFromRange(0, width-1);
+                y = getRandomNumberFromRange(0, height-1);
+                console.log(placeableCoords);
+            } while (placeableCoords[x * width + y] === false);
+            placeableCoords[x * width + y] = false;
+            changeEventPostion(eventId, x + regionPlace[0], y + regionPlace[2]);
+        });
+    }
+
+    // 插件命令：随机摆放物品
+    PluginManager.registerCommand(pluginName, 'randomPlacement', args => {
+        const regionPlace = JSON.parse(args.regionPlace).map(Number);
+        const items = JSON.parse(args.items);
+        randomPlaceItems(regionPlace, items);
     });
 
 })();
+
